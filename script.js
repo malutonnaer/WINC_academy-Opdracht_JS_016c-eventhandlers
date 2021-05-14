6061b9722d9c75929a7cb14a028d782ae6745f0a
malutonnaer <81236935+malutonnaer@users.noreply.github.com>
2021-05-14 13:44:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+var buttonAlert = document.getElementById('alert-button');
+buttonAlert.addEventListener("click", makeAlert);
+
+function makeAlert() {
+    alert("Button Clicked")
+};
+
 var buttonBackground = document.getElementById('background-button');
 const bodyElement = document.body;
 
@@ -5,3 +12,5 @@ buttonBackground.addEventListener("click", toggleBackground);
 function toggleBackground() {   
     bodyElement.classList.toggle("red-background");
 };
+
+
